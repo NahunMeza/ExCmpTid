698602a78b7039a88940dd3984ed936129e938d7
RonyMeza <rnahunmeza@gmail.com>
2022-01-25 22:30:11 -0600

    sesion

---
+++ SesionON.php
@@ -0,0 +1,26 @@
+<?php
+include("ConexLog.php");
+session_start();
+$EmailCliente =$_POST['EmailCliente'];
+$userPass  =$_POST['userPass'];
+$query  = "SELECT  userTipo,COUNT(*) AS verificaUser from tbcliente  where Email =  '$EmailCliente' and 	userPass =  '$userPass'";
+
+$resultado = mysqli_query($conexion,$query);
+$array =  mysqli_fetch_array($resultado);
+
+if ($array['verificaUser']>0   && $array['userTipo'] == 'Admin' ) {
+	$_SESSION['EmailCliente'] = $EmailCliente;
+include("UserOn.php");
+    header("location:https://nahummeza.000webhostapp.com/ExComp/home.php");
+}
+if ($array['verificaUser']>0   && $array['userTipo'] == 'Cliente' ) {
+	include("UserOn.php");
+	$_SESSION['EmailCliente'] = $EmailCliente;
+    header("location:https://nahummeza.000webhostapp.com/ExComp/home.php");
+}
+
+else {
+	echo "Usuario o Password Incorrectos.......";
+}
+
+ ?>
