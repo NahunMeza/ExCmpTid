adadd9c032ab8f57db1740d99d55643d2f71bde9
RonyMeza <rnahunmeza@gmail.com>
2022-01-26 23:20:07 -0600

    msj

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
-$mysqli = new mysqli("localhost", "nahunmez_Meza92", "coldplay1992", "nahunmez_Meganet" , 3306);
+$mysqli = new mysqli("localhost", "nahunmez_Meza92", "coldplay1992", "nahunmez_Tienda2" , 3306);
 if (mysqli_connect_errno()) {
 	echo "Conexion Fallida : " , mysqli_connect_errno();
 exit();
